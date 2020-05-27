c0e9d0b2eb573de704a3353e17dfd9e9af850d6a
Danny <danny@packer-output-bda7298a-d635-47f4-8b35-9156f6088fd7>
2020-05-27 12:40:34 +0000

    Current state of landing page development, without including the wallpaper bloat.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<head>
+  <title>Landing Page</title>
+
+  <link rel="stylesheet" href="CSS/main.css">
+
+  <style type="text/css">
+  	html {height:100%;background-image: url(<?php include 'PHP/rando_image.php';?>); background-position: center; background-size: 100% 100%;background-repeat: no-repeat;}
+  </style>
+</head>
+
+<body>
+	<p> This is the beginning </p>
+
+</body>
+</html>
